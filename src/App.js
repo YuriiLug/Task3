aa27f4fa08d7f1aa86c987272f90a21e4b4323d3
YuriiLug <lugyura7@gmail.com>
2021-12-21 12:09:10 +0200

    Add bootstrap and new element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
 
+class GridComponent extends React.Component {
+  render() {
+    return (
+<form>
+    <input placeholder="Filter By..." type="text" name="Filter By" />
+        <table  className="table table-condensed">
+                    <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Active</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>John</td>
+            <td>Doe</td>
+              <input type="checkbox" />
+          </tr>
+          <tr>
+            <td>Mary</td>
+            <td>Moe</td>
+              <input type="checkbox" />
+          </tr>
+          <tr>
+            <td>July</td>
+            <td>Dooley</td>
+              <input checked={true} type="checkbox" />
+          </tr>
+          </tbody>
+        </table>
+</form>
+    )
+  }
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <GridComponent/>;
 }
 
 export default App;
